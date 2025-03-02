4c4d2fc6ce5d2ead9b7cb9f766c953f7fb821d75
astopo <amy.soetopo@gmail.com>
2025-03-02 12:49:45 -0600

    feat: api for create note, get user with notes

--- code/server/src/index.ts
+++ code/server/src/index.ts
@@ -1,4 +1,5 @@
 import { AppDataSource } from "./data-source";
+import { Note } from "./entity/Note";
 import { User } from "./entity/User";
 import * as express from "express";
 
@@ -7,8 +8,18 @@ const run = async () => {
     const app = express();
     app.use(express.json());
     app.get("/users", async (req, res) => {
-        const users = await AppDataSource.manager.getRepository(User).find();
+        const users = await AppDataSource.manager.getRepository(User).find({
+            relations: {
+                notes: true
+            }
+        });
         res.json(users);
+    });
+    app.get("/users/:id", async (req, res) => {
+        const user = await AppDataSource.manager
+            .getRepository(User)
+            .findOne({ where: { id: req.params.id }, relations: { notes: true } });
+        res.json(user);
     });
     app.post("/users", async (req, res) => {
         const user = new User();
@@ -28,6 +39,13 @@ const run = async () => {
         user.phoneNumber = req.body.phoneNumber;
         await AppDataSource.manager.getRepository(User).save(user);
         res.json(user);
+    });
+    app.post("/users/:id/notes", async (req, res) => {
+        const note = new Note();
+        note.content = req.body.content;
+        note.user = req.params.id;
+        await AppDataSource.manager.getRepository(Note).save(note);
+        res.json(note);
     });
     app.listen(3000, () => {
         console.log("Server is running on http://localhost:3000");
